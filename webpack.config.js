b33d694de03e104187a4b8e2c4cad0dafe723cdd
Julian Xhokaxhiu <xhokaxhiujulian@gmail.com>
2017-11-01 18:44:14 +0100

    Enhance development experience
    And merge webpack.dev.js + webpack.prod.js in webpack.config.js from now on.
    The development profile will include a sourcemap in order to have a detailed stack trace during development.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,61 @@
-module.exports = function( env ) {
-  // Default to dev if no environment given
-  if ( !env ) env = 'dev'
+const path = require('path'),
+      webpack = require('webpack'),
+      package = require('./package.json')
+      BabiliPlugin = require('babili-webpack-plugin'),
+      WebpackSourceMapSupport = require('webpack-source-map-support')
 
-  return require(`./webpack.${env}.js`)
+module.exports = function( env ) {
+  const filename = package.main.replace( 'dist/', '' ),
+        libraryName = filename.replace( '.js', '' )
+
+  let config = {
+    entry: './src/',
+    target: 'node',
+    node: {
+      __dirname: false,
+      __filename: false,
+    },
+    stats: {
+      warnings: false
+    },
+    resolve: {
+      extensions: [ '.js' ]
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: filename,
+      library: libraryName,
+      libraryTarget: 'umd2'
+    },
+    plugins: []
+  }
+
+  if ( !env ) {
+    // ---- DEV ----
+    config.devtool = 'inline-source-map'
+
+    Array.prototype.push.apply(
+      config.plugins,
+      [
+        new WebpackSourceMapSupport()
+      ]
+    )
+  } else {
+    // ---- PROD ----
+    Array.prototype.push.apply(
+      config.plugins,
+      [
+        new webpack.LoaderOptionsPlugin({
+          minimize: true,
+          debug: false
+        }),
+        new BabiliPlugin({}, {
+          comments: false,
+          sourceMap: false
+        })
+      ]
+    )
+  }
+
+  return config
 }
