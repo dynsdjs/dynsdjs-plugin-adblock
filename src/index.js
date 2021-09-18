2f63b5ef5722122535e165930df8599f239c6971
Julian Xhokaxhiu <info@julianxhokaxhiu.com>
2021-09-18 11:11:24 +0200

    Improve the foreach logic using native loops instead of callbacks
    Additionally fix the resolve logic that was causing a crash

--- src/index.js
+++ src/index.js
@@ -10,48 +10,44 @@ function start( resolve, reject, data ) {
   fetch( rulesUrl )
     .then( res => res.text() )
     .then( body => {
-        body
-          .split('\n')
-          .forEach(
-            line => {
-              if ( !line.startsWith( '#' ) ) {
-                const host = line.split( /\s+/ )
-                let domain = ''
+        try {
+          for (const line of body.split('\n'))
+          {
+            if ( !line.startsWith( '#' ) && line.trim().length > 0 ) {
+              const host = line.split( /\s+/ )
+              let domain = ''
 
-                if ( host.length < 2 )
-                  domain = host[0]
-                else
-                  domain = host[1]
+              if ( host.length < 2 )
+                domain = host[0]
+              else
+                domain = host[1]
 
-                data.entries
-                  .set(
-                    domain,
-                    {
-                      A: {
-                        name: domain,
-                        address: ip.address( 'private', 'ipv4' ),
-                        ttl: 600
-                      },
-                      AAAA: {
-                        name: domain,
-                        // Logic is swapped. public returns a private ip.
-                        // TODO: fix when https://github.com/indutny/node-ip/issues/83 is solved.
-                        address: ip.address( 'public', 'ipv6' ),
-                        ttl: 600
-                      }
+              data.entries
+                .set(
+                  domain,
+                  {
+                    A: {
+                      name: domain,
+                      address: ip.address( 'private', 'ipv4' ),
+                      ttl: 600
+                    },
+                    AAAA: {
+                      name: domain,
+                      // Logic is swapped. public returns a private ip.
+                      // TODO: fix when https://github.com/indutny/node-ip/issues/83 is solved.
+                      address: ip.address( 'public', 'ipv6' ),
+                      ttl: 600
                     }
-                  )
-              }
+                  }
+                )
             }
-          )
+          }
 
-        data.entries
-          .keys(
-            ( err, keys ) => {
-              console.log( `[${chalk.blue('ADBLOCK')}] ${chalk.green(keys.length)} rules fetched.` )
-              resolve()
-            }
-          )
+          console.log( `[${chalk.blue('ADBLOCK')}] ${chalk.green(data.entries.keys().length)} rules fetched.` )
+          resolve()
+        } catch (ex) {
+          reject(ex)
+        }
       }
     )
 }
